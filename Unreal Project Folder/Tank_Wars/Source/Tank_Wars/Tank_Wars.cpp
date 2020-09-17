e6af1568e695ef41984d756ce0b46cc3af50dc2f
adingess <arvol.dingess@gmail.com>
2020-09-17 11:28:27 -0500

    Terrain Basics

---
+++ Unreal Project Folder/Tank_Wars/Source/Tank_Wars/Tank_Wars.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tank_Wars.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tank_Wars, "Tank_Wars" );
---
+++ Unreal Project Folder/Tank_Wars/Source/Tank_Wars/Tank_WarsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tank_WarsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_WARS_API ATank_WarsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
